5f7f64b6c105fc050d783a90175b42f77ddb915c
Virgil <lunguvirgil3000@gmail.com>
2022-10-06 13:31:00 +0300

    replace lat and long with city name

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const weatherButton = document.getElementById('weather')
 
-async function getWeatherData() {
-    const lat = document.getElementById('latitude').value;
-    const long = document.getElementById('longitude').value;
+async function showInfo() {
+    const weatherData = await getWeatherData();
+    await function component() {
+        const element = document.createElement('div');
+        element.className = 'weatherinfo';
+        element.textContent = `<p>The weather right now in your location is as follows:</p>
+            Temperature: ${(weatherData[0] - 273.15)}
+            Weather: ${weatherData[1]}
+            Wind: ${weatherData[2]}
+        <p>Don't forget to go outside for a walk whenever your mental health is in trouble.</p>
+        `
+        return element;
+    }
+}
 
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=30f8c65d65be94be78fe5fc8b7cbfe8f');
+
+async function getWeatherData() {
+    const city = document.getElementById('city').value
+
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=1586ba39a39b0e4ae17c264588d43487');
     const responseData = await response.json();
     const weatherInfo = [responseData.main.temp, responseData.weather[0].description, responseData.wind.speed];
     console.log(weatherInfo);
+    return weatherInfo;
 }
 
-weatherButton.addEventListener('click', (e) => {
-    getWeatherData();
+weatherButton.addEventListener('click', () => {
+    showInfo();
+    document.appendChild(component());
 })
