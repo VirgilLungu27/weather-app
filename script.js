70e744979e1ef02802f2fcac261c37cfa5dbd510
Virgil <lunguvirgil3000@gmail.com>
2022-10-08 20:23:40 +0300

    add style and inserted city in the weatherInfo object

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ async function getWeatherData() {
     const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=1586ba39a39b0e4ae17c264588d43487');
     const responseData = await response.json();
     const weatherInfo = await {
+        'city': city,
         'temperature': responseData.main.temp,
         'weather': responseData.weather[0].description,
         'wind': responseData.wind.speed
@@ -20,11 +21,11 @@ async function component() {
 
     const element = document.createElement('div');
     element.className = 'weatherinfo';
-    element.innerHTML = `<div class="hero"><h2>The weather right now in your location is as follows:</h2>
+    element.innerHTML = `<div class="hero"><h2>The weather right now in ${weatherData.city} is as follows:</h2>
         <div class="temp"><h3>Temperature: ${(weatherData.temperature - 273.15).toFixed(1)} °C </h3></div>
         <div class="weather"><h3>Weather: ${weatherData.weather}</h3></div>
         <div class="wind"><h3>Wind: ${weatherData.wind} m/s</h3></div>
-        <h4><i>Go outside for a walk whenever you feel like you need to manage your mental health.</i></h4></div>`
+        <h5><i>Go outside for a walk whenever you feel like you need to manage your mental health.</i></h5></div>`
     return element;
 }
 
