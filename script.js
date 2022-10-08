88d54331d027401a6221847d2facfe8af307e641
Virgil <lunguvirgil3000@gmail.com>
2022-10-08 18:17:18 +0300

    Succesfully fetched data in the inner html

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const weatherButton = document.getElementById('weather')
+const weatherDiv = document.getElementById('weatherinfo')
 
 // Function to fetch information through an async/await mechanism
 async function getWeatherData() {
@@ -6,29 +7,32 @@ async function getWeatherData() {
 
     const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=1586ba39a39b0e4ae17c264588d43487');
     const responseData = await response.json();
-    const weatherInfo = {
-        temperature: responseData.main.temp,
-        weather: responseData.weather[0].description,
-        wind: responseData.wind.speed
+    const weatherInfo = await {
+        'temperature': responseData.main.temp,
+        'weather': responseData.weather[0].description,
+        'wind': responseData.wind.speed
     };
     return(weatherInfo);
 }
 
 // Component to append weather info.
 async function component() {
-    const element = document.createElement('div');
-    element.className = 'weatherinfo';
     const weatherData = await getWeatherData();
-    element.textContent = `
-        <p>The weather right now in your location is as follows:</p>
-        Temperature: ${(weatherData[0] - 273.15)}
-        Weather: ${weatherData[1]}
-        Wind: ${weatherData[2]}
-        <p>Don't forget to go outside for a walk whenever your mental health is in trouble.</p>
+
+    const element = await document.createElement('div');
+    element.className = await 'weatherinfo';
+    element.textContent = await `
+        The weather right now in your location is as follows: \n
+        Temperature: ${(weatherData.temperature - 273.15)} \n
+        Weather: ${weatherData.weather} \n
+        Wind: ${weatherData.wind} \n
+        Don't forget to go outside for a walk whenever your mental health is in trouble.
     `
     return element;
 }
 
-weatherButton.addEventListener('click', () => {
-    console.log(component())
+weatherButton.addEventListener('click', async function appendComponent() {
+    koko = await component()
+    console.log(koko)
+    weatherDiv.appendChild(koko)
 })
